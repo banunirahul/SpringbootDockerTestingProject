583b126778217812ed6d0b0bee7f224b85437315
banunirahul <banunirahul03@gmail.com>
2024-12-04 10:46:31 +0530

    Code changes to add springboot project

---
+++ src/main/java/com/springboot/dockerDeployment/DockerDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.dockerDeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/dockerDeployment/DockerDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.dockerDeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
